677d551129521c58abd915c9c7788d568c26a3a6
Valerio Farina <valerio.farina.1994@gmail.com>
2020-10-22 22:24:54 +0200

    Aggiunto il bonus: l'utente può scegliere il livello di difficoltà

--- main.js
+++ main.js
@@ -1,19 +1,49 @@
 /*  CONSEGNA: Il programma deve generare 16 numeri compresi tra 1 e 100: queste saranno le mine.
     Dopodiché, il programma deve chiedere all'utente un numero alla volta e verificare se il numero indicato dall'utente è una mina oppure no.
     Se l'utente becca una mina, il gioco finisce, mentre, se il numero non corrisponde ad una mina, il gioco prosegue e il programma chiede all'utente un nuovo numero.
-    Alla fine della partita, il programma comunica all'utente il suo punteggio, cioè quanti numeri è riuscito ad inserire prima che il gioco finisse.  */
+    Alla fine della partita, il programma comunica all'utente il suo punteggio, cioè quanti numeri è riuscito ad inserire prima che il gioco finisse.
+    BONUS (facoltativo): all'inizio del gioco, il programma chiede all'utente il livello di difficoltà:
+        - 0 = l'intervallo di numeri possibili è tra 1 e 100
+        - 1 = l'intervallo di numeri possibili è tra 1 e 80
+        - 2 = l'intervallo di numeri possibili è tra 1 e 50
+    In ogni caso, le mine sono sempre 16.  */
+
 
 
 // creo una variabile contenente come valore il numero delle mine che desideriamo generare
 var numberOfMines = 16;
 
-// creo un array (vuoto) in cui andrò a salvare le mine generate
-var mines = [];
+// chiedo all'utente di inserire il livello di difficoltà
+// continuo a chiderglielo fintanto che l'input da lui inserito non è corretto
+// in questo caso, l'input dell'utente non è corretto quando si verifica almeno una delle seguenti condizioni:
+    // - l'input inserito non è un numero
+    // - l'input inserito è un numero, ma non è nè 0, nè 1, nè 2 (cioè, è < 0 oppure > 2)
+var level;
+do {
+    level = parseInt(prompt('Scegli un livello di difficoltà tra 0, 1 e 2'));
+    if (isNaN(level) || level < 0 || level > 2) {
+        alert('Per favore, inserisci 0, 1 oppure 2');
+    }
+} while(isNaN(level) || level < 0 || level > 2);
 
 // creo due variabili inf e sup (estremo inferiore ed estremo superiore)
 // sia le mine sia i numeri inseriti dall'utente dovranno essere compresi tra inf e sup
 var inf = 1;
-var sup = 100;
+var sup;
+
+// controllo quale livello ha inserito l'utente
+// a seconda del livello, stabilisco quale valore assegnare a sup
+// il valore di inf rimane invece sempre uguale a 1
+if (level == 0) {
+    sup = 100;
+} else if (level == 1) {
+    sup = 80;
+} else if (level == 2) {
+    sup = 50;
+}
+
+// creo un array (vuoto) in cui andrò a salvare le mine generate
+var mines = [];
 
 // finché il numero di mine salvate in mines (cioè, la lunghezza dell'array mines) è inferiore al numero desiderato (cioè, inferiore a numberOfMines), continuo a generare nuove mine
 while (mines.length < numberOfMines) {
@@ -37,7 +67,7 @@ var mineFound = false;
 
 // creo una variabile maxScore che rappresenta il massimo punteggio che l'utente può realizzare
 // l'utente realizza il punteggio massimo quando inserisce uno dopo l'altro esattamente tutti quei numeri compresi tra inf e sup che non sono presenti nell'array mines
-// il punteggio massimo corrisponde quindi alla quantità di numeri compresi tra inf e sup che diversi dalle mine
+// il punteggio massimo corrisponde quindi alla quantità di numeri compresi tra inf e sup e che sono diversi dalle mine
 // quindi, per calcolare il punteggio massimo dobbiamo:
     // - calcolare quanti sono i numeri compresi tra inf e sup (questo si ottiene facendo sup - inf + 1)
     // - sottrarre a tale valore il numero delle mine
@@ -54,14 +84,15 @@ var userScore = userNumbers.length;
     // - mineFound ha valore false, e quindi l'utente non ha beccato una mina (cioè, non ha inserito un numero presente nell'array mines)
     // - il punteggio totalizzato dall'utente non ha raggiunto il valore massimo
 do {
-    // tuttavia, prima di controllare se l'utente ha beccato una mina, controllo se l'input dell'utente è corretto
-    // in altre parole, controllo se l'input dell'utente è un numero compreso tra inf e sup
+    var userNumber;
     do {
-        var userNumber = parseInt(prompt('Inserisci un numero compreso tra ' + inf + ' e ' + sup));
+        userNumber = parseInt(prompt('Inserisci un numero compreso tra ' + inf + ' e ' + sup));
+        // prima di controllare se l'utente ha beccato una mina, controllo se l'input dell'utente è corretto
+        // in altre parole, controllo se l'input dell'utente è un numero compreso tra inf e sup
         if (isNaN(userNumber) || userNumber < inf || userNumber > sup) {
             alert('Per favore, inserire un numero compreso tra ' + inf + ' e ' + sup);
         }
-    } while (isNaN(userNumber) || userNumber < inf || userNumber > sup)
+    } while (isNaN(userNumber) || userNumber < inf || userNumber > sup);
     // controllo se l'utente ha beccato una mina (cioè, se il numero che ha appena inserito è incluso nell'array mines)
     if (mines.includes(userNumber)) {
         // se l'utente ha beccato una mina, pongo mineFound uguale a true (questo rende la condizione di permanenza nel ciclo falsa)
